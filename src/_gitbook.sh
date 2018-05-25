464a744fd48658912e93c011a69793e056b1454e
Régis Frias <regisfrias@gmail.com>
2018-05-25 10:46:46 +0300

    Add images to gitbook folder. Now sh cmd can be run with existing dist folder.

--- src/_gitbook.sh
+++ src/_gitbook.sh
@@ -1,4 +1,4 @@
-mkdir "../dist/gitbook" && cp 00-introduction.md ../dist/gitbook/README.md && touch ../dist/gitbook/SUMMARY.md;
+mkdir -p "../dist" && mkdir -p "../dist/gitbook" && mkdir -p "../dist/gitbook/img" && cp 00-introduction.md ../dist/gitbook/README.md && rm ../dist/gitbook/SUMMARY.md && touch ../dist/gitbook/SUMMARY.md;
 echo "# Summary" >> ../dist/gitbook/SUMMARY.md;
 for f in *.md ;
   do if [ "${f}" != "00-introduction.md" ];
@@ -8,6 +8,9 @@ done;
 sed -ie 's/ # / /g' ../dist/gitbook/SUMMARY.md &&
 for f in *.md ;
   do pandoc -f markdown ${f} --template=template.html -t html -s -o ../dist/gitbook/${f}.html  ;
+done &&
+for f in img/* ;
+  do cp ${f} ../dist/gitbook/${f};
 done &&
 for f in *.md ;
   do echo "{% include \"${f}.html\" %}" > ../dist/gitbook/${f}  ;
